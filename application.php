091748513e1d38afaad95f34fd6e8399eaa7a67a
Modern-Socialist-Community <42880541+Modern-Socialist-Community@users.noreply.github.com>
2018-09-02 09:57:08 -0500

    Added all Liubishuhuland files
    Added CharisSIL fonts, jQuery-3.2.1.min.js, application.php, w3.css, CourtOrder.pdf, flag_border.png, and index.php, which will replace index.html.

---
+++ application.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="regular.css">
+	<script src="jquery-3.2.1.min.js"></script>
+
+	<script>
+	$(document).ready(function(){
+	        $("#title").fadeIn(1000);
+	        $("#subtitle").fadeIn(1500);
+		$("#joinButton").fadeIn(2000);
+		$("#mainText").fadeIn(2500);
+	});
+	</script>
+</head>
+<body>
+
+<h1 id="title">L&#1102;&#774;bi&#768;&#1096;u&#768;xu&#774;t&#603;&#771;su&#771;</h1>
+
+<h2 id="subtitle">Su&#774; T&#603;&#771;su&#771; O&#771;jo &#936;y&#771;&#1097;den Si&#771;&#893;&#603;</h2>
+
+<div style="text-align:center;" id="joinButton">
+<a href="CourtOrder.pdf"><button type="button" class="pressable">&#1044;ua&#769;&#7545;</button></a>
+</div>
+
+<p class="regular" id="mainText" style="display:none;">
+L&#1102;&#774;bi&#768;&#1096;u&#768;xu&#774;t&#603;&#771;su&#771; ja&#771; y&#771; la&#768; m&#1099;&#771;&#1097; my&#603;&#771;n &#948;a&#769;n&#999;&#601;&#768;et un &#956;u&#769;&#966;u&#768; jo dz&#603;&#771;nmo&#774;i t&#603;&#771;su&#771;.
+
+</p>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="w3.css">
+	<link rel="stylesheet" type="text/css" href="regular.css">
+	<script src="jquery-3.2.1.min.js"></script>
+
+	<script>
+	$(document).ready(function(){
+		$("#navigation").fadeIn(500);
+	    $("#title").fadeIn(1000);
+	    $("#subtitle").fadeIn(1500);
+		$("#joinButton").fadeIn(2000);
+		$("#mainText").fadeIn(2500);
+	});
+	</script>
+	<style>
+	html, body, h1, h2, h3, h4, h5, h6 {
+  		font-family: lbsh, cambria;
+	}
+	</style>
+</head>
+<body>
+
+<div id="navigation" class="w3-bar w3-yellow">
+  <a href="#" class="w3-bar-item w3-button">Home</a>
+  <a href="#" class="w3-bar-item w3-button">Link 1</a>
+  <a href="#" class="w3-bar-item w3-button">Link 2</a>
+  <a href="#" class="w3-bar-item w3-button">Link 3</a>
+</div>
+
+<h1 id="title">Liubishuhuland</h1>
+
+<h2 id="subtitle">The Land Of Golden Rays</h2>
+
+<div style="text-align:center;" id="joinButton">
+<a href="application.php"><button type="button" class="pressable">Apply</button></a>
+</div>
+
+<p class="regular" id="mainText" style="display:none;">
+Liubishuhuland is a modern-day socio-politico-economic entity founded in 2010. As it exhibits characteristics of historical Utopian communities, Liubishuhuland is often referred to as one. The mission of Liubishuhuland is to improve the lives of its members through its unique culture and society and a specially designed Cooperative Commonwealth government-economy.
+<br/><br/>
+Geographically, Liubishuhuland is a collection of scattered sites overseen by one central location. The locations of Liubishuhuland are only disclosed to prospective members who advance far enough in the application process.
+<br/><br/>
+Application for Membership is a difficult process designed to wear off the resolve of all candidates except for the most dedicated. The application process is organized into 4 phases, each described below:
+<br/><br/><b>Phase 1</b><br/>
+Each applicant will be provided the Liubishuhu Language Guide, the Liubishuhu-English Dictionary, and the Application for Membership. Once the Application for Membership is completed and mailed or submitted digitally to the Admissions Office, reviewers will inform the applicant whether the application can be continued.
+<br/><br/><b>Phase 2</b><br/>
+Each applicant who passes Phase 1 will be provided an identification code and an written examination over Liubishuhuland, the Liubishuhu language, mathematics, English, and general knowledge. Upon its completion and submission to the Admissions Office, the examination will be scored by reviewers, who will inform the applicant whether the application can be continued.
+<br/><br/><b>Phase 3</b><br/>
+Each applicant who passes Phase 2 will be invited to a physical or online interview with a member. After a successful interview, the applicant will be provided a list of locations where he/she can enter into a temporary residence.
+<br/><br/><b>Phase 4</b><br/>
+The applicant will enter into residence, during which he/she will be observed by the community until deemed worthy of full membership.
+</p>
+
+</body>
+</html>
